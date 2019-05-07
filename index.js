d2b93b59a22dc638d071415b83a011180ea73992
zep1994 <snaxbags@protonmail.com>
2019-05-07 10:30:38 -0400

    init html/css/JS

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-let addTodo = document.getElementById('addTodo')
-let inputTodo = document.getElementById('todoInput')
-let listTodo = document.getElementById('todoList')
-
-addTodo.addEventListener("click", function() {
-     let text = inputTodo.value;
-     inputTodo.value = ""
-     let li = document.createElement('li');
-     li.innerHTML = text
-     let removeTodo = createElement("button");
-     removeTodo.classList.add("remove-btn");
-     removeTodo.innerHTML = "HTML";
-     remove
-     listTodo.appendChild(li);
-     
-})
+let nodeList = document.getElementsByTagName('li')
+let i; 
